64a40249512fe8014ec7efbfe9d72bf6d34051a0
jsdpatrykfijalkowski <patryk.fijalkowski@stxnext.pl>
2020-05-21 16:39:45 +0200

    Added prettier config to project

--- frontend/src/components/NewReceiptForm/NewReceiptForm.js
+++ frontend/src/components/NewReceiptForm/NewReceiptForm.js
@@ -1,51 +1,54 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
 
-import {Field, FieldArray, Formik, useFormik, FormikProps, useField, useFormikContext} from 'formik';
-import {useTranslation} from "react-i18next";
+import { FieldArray, Formik, useField, useFormikContext } from "formik";
+import { useTranslation } from "react-i18next";
 import Form from "react-bootstrap/Form";
-import {TextField} from "@material-ui/core";
+import { TextField } from "@material-ui/core";
 import { Button } from "@material-ui/core";
-import {generate} from 'shortid';
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from '@material-ui/icons/Delete';
-import Autocomplete from '@material-ui/lab/Autocomplete';
+import DeleteIcon from "@material-ui/icons/Delete";
+
+import Autocomplete from "@material-ui/lab/Autocomplete";
 
 export const DatePickerField = ({ ...props }) => {
-    const {setFieldValue} = useFormikContext();
+    const { setFieldValue } = useFormikContext();
     const [field] = useField(props);
-
 
     return (
         <DatePicker
             {...field}
             {...props}
             selected={(field.value && new Date(field.value)) || null}
-            onChange={val => {
-                setFieldValue(field.name, val)
+            onChange={(val) => {
+                setFieldValue(field.name, val);
             }}
             placeholderText="Data"
             autoComplete="off"
             customInput={<TextField variant="outlined" />}
         />
-    )
-}
+    );
+};
 
 const NewReceiptForm = () => {
-    const {t} = useTranslation();
-    const [itemValues, setItemValues] = useState({itemName: "", itemPrice: "", itemCategory: ""})
+    const { t } = useTranslation();
+    const [itemValues, setItemValues] = useState({
+        itemName: "",
+        itemPrice: "",
+        itemCategory: "",
+    });
 
     const top100Films = [
-        { title: 'Artykuły spożywcze' },
-        { title: 'Kosmetyki' },
-        { title: 'Transport' },
-        { title: 'Mieszkanie'}
+        { title: "Artykuły spożywcze" },
+        { title: "Kosmetyki" },
+        { title: "Transport" },
+        { title: "Mieszkanie" },
     ];
     const defaultProps = {
         options: top100Films,
@@ -60,52 +63,72 @@ const NewReceiptForm = () => {
                     recipeAmount: null,
                     shopName: null,
                     items: [
-                        {itemName: "Jajka", itemPrice: "22,99", itemCategory: "Artykuły spożywcze"},
-                        {itemName: "Chleb", itemPrice: "1,99", itemCategory: "Artykuły spożywcze"},
-                        {itemName: "Woda", itemPrice: "2,99", itemCategory: "Artykuły spożywcze"},
-                        {itemName: "Szampon", itemPrice: "11,99", itemCategory: "Kosmetyki"},
-                    ]
+                        {
+                            itemName: "Jajka",
+                            itemPrice: "22,99",
+                            itemCategory: "Artykuły spożywcze",
+                        },
+                        {
+                            itemName: "Chleb",
+                            itemPrice: "1,99",
+                            itemCategory: "Artykuły spożywcze",
+                        },
+                        {
+                            itemName: "Woda",
+                            itemPrice: "2,99",
+                            itemCategory: "Artykuły spożywcze",
+                        },
+                        {
+                            itemName: "Szampon",
+                            itemPrice: "11,99",
+                            itemCategory: "Kosmetyki",
+                        },
+                    ],
                 }}
                 onSubmit={(values, actions) => {
-                    console.log(values)
-                }
-                }
-                render={({ values, handleSubmit, handleChange, handleReset, dirty, isSubmitting  }) => (
+                    console.log(values);
+                }}
+                render={({ values, handleSubmit, handleChange, handleReset, dirty, isSubmitting }) => (
                     <Form onSubmit={handleSubmit}>
                         <TextField
                             id="standard-full-width"
                             variant="outlined"
-                            name='shopName'
-                            placeholder='Nazwa sklepu'
-                            type='text'
+                            name="shopName"
+                            placeholder="Nazwa sklepu"
+                            type="text"
                             onChange={handleChange}
                         />
                         <DatePickerField name="date" />
                         <TextField
                             variant="outlined"
-                            name='recipeAmount'
-                            placeholder='Kwota całkowita PLN'
-                            type='number'
+                            name="recipeAmount"
+                            placeholder="Kwota całkowita PLN"
+                            type="number"
                             onChange={handleChange}
                         />
                         <FieldArray
                             name="items"
-                            render={arrayHelpers => (
+                            render={(arrayHelpers) => (
                                 <div>
                                     <TextField
                                         variant="outlined"
-                                        name='itemName'
+                                        name="itemName"
                                         placeholder="Nazwa produktu"
-                                        type='text'
-                                        onChange={(e) => setItemValues({...itemValues, itemName: e.target.value})}
+                                        type="text"
+                                        onChange={(e) => setItemValues({ ...itemValues, itemName: e.target.value })}
                                         value={itemValues.itemName}
                                     />
                                     <TextField
                                         variant="outlined"
-                                        name='itemPrice'
+                                        name="itemPrice"
                                         placeholder="Cena"
-                                        type='number'
-                                        onChange={(e) => setItemValues({...itemValues, itemPrice: e.target.value})}
+                                        type="number"
+                                        onChange={(e) =>
+                                            setItemValues({
+                                                ...itemValues,
+                                                itemPrice: e.target.value,
+                                            })
+                                        }
                                         value={itemValues.itemPrice}
                                     />
                                     <Autocomplete
@@ -118,37 +141,55 @@ const NewReceiptForm = () => {
                                                 {...params}
                                                 variant="outlined"
                                                 label="Kategoria"
-                                            name='itemCategory'
-                                            placeholder="Kategoria"
-                                            type='test'
-                                            onChange={(e) => setItemValues({...itemValues, itemCategory: e.target.value})}
-                                            value={itemValues.itemCategory}
+                                                name="itemCategory"
+                                                placeholder="Kategoria"
+                                                type="test"
+                                                onChange={(e) =>
+                                                    setItemValues({
+                                                        ...itemValues,
+                                                        itemCategory: e.target.value,
+                                                    })
+                                                }
+                                                value={itemValues.itemCategory}
                                             />
                                         )}
                                     />
 
-                                    <Button variant="outlined" type='button' onClick={()=> {
-                                        setItemValues({itemName: "", itemPrice: "", itemCategory: ""})
-                                        arrayHelpers.push(itemValues)
-                                    }}>
+                                    <Button
+                                        variant="outlined"
+                                        type="button"
+                                        onClick={() => {
+                                            setItemValues({
+                                                itemName: "",
+                                                itemPrice: "",
+                                                itemCategory: "",
+                                            });
+                                            arrayHelpers.push(itemValues);
+                                        }}>
                                         Add item
                                     </Button>
-                                    <List >
-                                        {values.items && values.items.length > 0 ? values.items.map((item, index) => {
-
-                                            return (
-                                                <ListItem key={index} role={undefined} dense button onClick={(value) => console.log(value)}>
-                                                    <ListItemText id={index} primary={item.itemName} />
-                                                    <ListItemText id={index} primary={item.itemPrice} />
-                                                    <ListItemText id={index} primary={item.itemCategory} />
-                                                    <ListItemSecondaryAction>
-                                                        <IconButton edge="end" aria-label="delete">
-                                                            <DeleteIcon />
-                                                        </IconButton>
-                                                    </ListItemSecondaryAction>
-                                                </ListItem>
-                                            );
-                                        }) : null}
+                                    <List>
+                                        {values.items && values.items.length > 0
+                                            ? values.items.map((item, index) => {
+                                                  return (
+                                                      <ListItem
+                                                          key={index}
+                                                          role={undefined}
+                                                          dense
+                                                          button
+                                                          onClick={(value) => console.log(value)}>
+                                                          <ListItemText id={index} primary={item.itemName} />
+                                                          <ListItemText id={index} primary={item.itemPrice} />
+                                                          <ListItemText id={index} primary={item.itemCategory} />
+                                                          <ListItemSecondaryAction>
+                                                              <IconButton edge="end" aria-label="delete">
+                                                                  <DeleteIcon />
+                                                              </IconButton>
+                                                          </ListItemSecondaryAction>
+                                                      </ListItem>
+                                                  );
+                                              })
+                                            : null}
                                     </List>
                                     {/*<ul>*/}
                                     {/*    {values.items && values.items.length > 0 ? (*/}
@@ -159,7 +200,9 @@ const NewReceiptForm = () => {
                                     {/*</ul>*/}
 
                                     <div>
-                                        <Button variant="outlined" type="submit">Submit</Button>
+                                        <Button variant="outlined" type="submit">
+                                            Submit
+                                        </Button>
                                     </div>
                                 </div>
                             )}
@@ -168,8 +211,7 @@ const NewReceiptForm = () => {
                 )}
             />
         </div>
-    )
-}
-
+    );
+};
 
 export default NewReceiptForm;
