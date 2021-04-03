9f8cf9ac318cd82a9a382413c040440bb505560a
PatrykFij <62008203+PatrykFij@users.noreply.github.com>
2021-04-03 07:59:22 +0200

    Delete frontend directory

--- frontend/src/store/reducers/RootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import accountReducer from "./AccountReducer";
-
-const rootReducer = combineReducers({
-    account: accountReducer
-});
-
-export default rootReducer;
--- frontend/src/utils/constans.js
+++
@@ -1,4 +0,0 @@
-export const regexps = {
-    NOT_ONLY_SPEC_CHAR_AND_NUMS: /(?!^[\d\W_]+$)^.{2,}$/,
-    ONLY_ZEROS: /^0*$/,
-};
